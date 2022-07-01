56c55cd66fcab61bc6b709bfcd9263a9f9560009
SuiAn <tiexinqin@gmail.com>
2022-07-01 21:13:29 +0800

    Delete test_argmax.py

--- test_argmax.py
+++
@@ -1,8 +0,0 @@
-import torch
-if __name__ == '__main__':
-    pred = torch.rand([10, 2]).float()
-    targets = torch.tensor([1, 0, 1, 1, 0, 0, 0, 1, 1, 0])
-    pred_y = torch.argmax(pred, dim=-1)
-
-    acc = torch.sum(pred_y == targets) / pred.size(0)
-    print(acc)
